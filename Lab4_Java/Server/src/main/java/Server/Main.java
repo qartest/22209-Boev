4238b5089d32bc6fbe7d72588dbae0a805fd1c65
qartest <boevtimofei@gmail.com>
2024-06-15 22:39:22 +0700

    Server and client. All work

--- Lab4_Java/Server/src/main/java/Server/Main.java
+++ Lab4_Java/Server/src/main/java/Server/Main.java
@@ -1,34 +1,29 @@
 package Server;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.DatagramChannel;
-import java.util.concurrent.TimeUnit;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        DatagramChannel chan = DatagramChannel.open();
-        chan.configureBlocking( false );
-        chan.bind( new InetSocketAddress( 7777 ) );
-//        var server = new Server();
-        ByteBuffer buffer = ByteBuffer.allocate( 4*1024 );
+        var server = new Server(getInetAddress(), 8081);
 
         while (true) {
-            buffer.clear();
-            System.out.println("trying non-blocking receive...");
-            SocketAddress from = chan.receive(buffer);
-            System.out.println("non-blocking receive done.");
-
-            if (from != null) {
-                buffer.flip();
-                System.out.printf("<<<--- got [%x] byte from %s%n", buffer.get(), from);
-            }
-
-            System.out.println( "sleeping..." );
-            TimeUnit.SECONDS.sleep( 5 );
+            server.start();
         }
+
     }
+
+    private static InetAddress getInetAddress(){
+        InetAddress addr;
+        try{
+            addr = InetAddress.getByName("127.0.0.1");
+        }catch (UnknownHostException e) {
+            addr = null;
+        }
+        return addr;
+    }
+
 }
